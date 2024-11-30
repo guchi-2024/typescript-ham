475dbf2b089e293b474e22b64bd0971961d8420e
guchi-2024 <163069131+guchi-2024@users.noreply.github.com>
2024-11-30 20:03:13 +0900

    generics.ts

---
+++ src/section9/type-compatibility.ts
@@ -0,0 +1,12 @@
+interface Animal {
+  age: number;
+  name: string;
+}
+
+class Person {
+  constructor(public age: number, public name: string) {}
+}
+
+let me: Animal;
+me = new Person(43, 'HamuSan');
+
