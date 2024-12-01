1804cdbaf8b4e1e79df234eae19d9b1e39cedfd6
guchi-2024 <163069131+guchi-2024@users.noreply.github.com>
2024-12-01 23:10:39 +0900

    type-assertions

---
+++ src/section9/rype-assertions.ts
@@ -0,0 +1,27 @@
+export{}
+
+let name: any = 'Ham';
+
+
+let length = (name as string).length;
+
+// length = 'foo';
+
+// const アサーション
+
+// let name = 'Atsushi';
+// name = 'Ham';
+
+// let nickName = 'Ham' as const
+// nickName = 'Ham';
+
+// let profile = {
+//   name: 'Atsushi',
+//   height: 178
+// } as const;
+
+// profile.name = 'Ham';
+// profile.height = 180;
+
+
+
